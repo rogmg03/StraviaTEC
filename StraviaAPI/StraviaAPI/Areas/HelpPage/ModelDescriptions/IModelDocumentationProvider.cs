06bb8338a1de2d79cc3206e8da8d5ad0693df501
Roger <rogermora03@gmail.com>
2020-11-18 00:41:14 -0600

    Views start

---
+++ StraviaAPI/StraviaAPI/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace StraviaAPI.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
